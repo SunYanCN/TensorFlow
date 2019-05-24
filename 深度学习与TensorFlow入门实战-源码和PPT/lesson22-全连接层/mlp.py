8246fd25036fe2a0a88e104fdb789e7334c5fc82
奔波儿 <li1191863273@hotmail.com>
2019-05-24 23:09:43 +0800

    chore: 优化结构

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson22-全连接层/mlp.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson22-全连接层/mlp.py
@@ -1,19 +1,15 @@
-import tensorflow as tf 
-from 	tensorflow import keras
-
-
-
-
+import tensorflow as tf
+from tensorflow import keras
 
 x = tf.random.normal([2, 3])
 
 model = keras.Sequential([
-		keras.layers.Dense(2, activation='relu'),
-		keras.layers.Dense(2, activation='relu'),
-		keras.layers.Dense(2)
-	])
+    keras.layers.Dense(2, activation='relu'),
+    keras.layers.Dense(2, activation='relu'),
+    keras.layers.Dense(2)
+])
 model.build(input_shape=[None, 3])
 model.summary()
 
 for p in model.trainable_variables:
-	print(p.name, p.shape)
+    print(p.name, p.shape)
