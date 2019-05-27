ea602d3bda02c0415090ed7ffa60f0637660ea5c
奔波儿 <li1191863273@hotmail.com>
2019-05-27 20:06:36 +0800

    chore: 优化结构

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson24-误差计算/loss.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson24-误差计算/loss.py
@@ -1,7 +1,4 @@
-import tensorflow as tf 
-
-
-
+import tensorflow as tf
 
 y = tf.constant([1, 2, 3, 0, 2])
 y = tf.one_hot(y, depth=4)
@@ -9,13 +6,11 @@ y = tf.cast(y, dtype=tf.float32)
 
 out = tf.random.normal([5, 4])
 
+loss1 = tf.reduce_mean(tf.square(y - out))
 
-loss1 = tf.reduce_mean(tf.square(y-out))
+loss2 = tf.square(tf.norm(y - out)) / (5 * 4)
 
-loss2 = tf.square(tf.norm(y-out))/(5*4)
-
-loss3 = tf.reduce_mean(tf.losses.MSE(y, out)) # VS MeanSquaredError is a class
-
+loss3 = tf.reduce_mean(tf.losses.MSE(y, out))  # VS MeanSquaredError is a class
 
 print(loss1)
 print(loss2)
