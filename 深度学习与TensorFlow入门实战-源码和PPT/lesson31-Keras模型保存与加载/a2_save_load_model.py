4f789ca7f8d4ef4ba876d46124d94fe7267c401c
奔波儿 <li1191863273@hotmail.com>
2019-05-29 23:20:47 +0800

    chore: 格式化代码

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson31-Keras模型保存与加载/a2_save_load_model.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson31-Keras模型保存与加载/a2_save_load_model.py
@@ -1,5 +1,6 @@
-import  tensorflow as tf
-from tensorflow.keras import datasets, layers, optimizers, Sequential
+import tensorflow as tf
+from tensorflow._api.v2.v2 import optimizers
+from tensorflow.python.keras import datasets, layers, Sequential
 
 
 def preprocess(x, y):
@@ -7,45 +8,39 @@ def preprocess(x, y):
     x is a simple image, not a batch
     """
     x = tf.cast(x, dtype=tf.float32) / 255.
-    x = tf.reshape(x, [28*28])
+    x = tf.reshape(x, [28 * 28])
     y = tf.cast(y, dtype=tf.int32)
     y = tf.one_hot(y, depth=10)
-    return x,y
+    return x, y
 
 
 batchsz = 128
 (x, y), (x_val, y_val) = datasets.mnist.load_data()
 print('datasets:', x.shape, y.shape, x.min(), x.max())
 
-
-
-db = tf.data.Dataset.from_tensor_slices((x,y))
+db = tf.data.Dataset.from_tensor_slices((x, y))
 db = db.map(preprocess).shuffle(60000).batch(batchsz)
 ds_val = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-ds_val = ds_val.map(preprocess).batch(batchsz) 
+ds_val = ds_val.map(preprocess).batch(batchsz)
 
 sample = next(iter(db))
 print(sample[0].shape, sample[1].shape)
 
-
 network = Sequential([layers.Dense(256, activation='relu'),
-                     layers.Dense(128, activation='relu'),
-                     layers.Dense(64, activation='relu'),
-                     layers.Dense(32, activation='relu'),
-                     layers.Dense(10)])
-network.build(input_shape=(None, 28*28))
+                      layers.Dense(128, activation='relu'),
+                      layers.Dense(64, activation='relu'),
+                      layers.Dense(32, activation='relu'),
+                      layers.Dense(10)])
+network.build(input_shape=(None, 28 * 28))
 network.summary()
 
-
-
-
 network.compile(optimizer=optimizers.Adam(lr=0.01),
-		loss=tf.losses.CategoricalCrossentropy(from_logits=True),
-		metrics=['accuracy']
-	)
+                loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy']
+                )
 
 network.fit(db, epochs=3, validation_data=ds_val, validation_freq=2)
- 
+
 network.evaluate(ds_val)
 
 network.save('model.h5')
@@ -55,11 +50,11 @@ del network
 print('loaded model from file.')
 network = tf.keras.models.load_model('model.h5', compile=False)
 network.compile(optimizer=optimizers.Adam(lr=0.01),
-        loss=tf.losses.CategoricalCrossentropy(from_logits=True),
-        metrics=['accuracy']
-    )
+                loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy']
+                )
 x_val = tf.cast(x_val, dtype=tf.float32) / 255.
-x_val = tf.reshape(x_val, [-1, 28*28])
+x_val = tf.reshape(x_val, [-1, 28 * 28])
 y_val = tf.cast(y_val, dtype=tf.int32)
 y_val = tf.one_hot(y_val, depth=10)
 ds_val = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(128)
