18f77aed579c22bc847262b08c9e653729139afe
奔波儿 <li1191863273@hotmail.com>
2019-05-29 21:11:21 +0800

    feat: 抽取工具

---
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson28-可视化/TensorBoardUtils.py
@@ -0,0 +1,40 @@
+import io
+
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+
+def plot_to_image(figure):
+    """
+    输出图片
+    Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    image = tf.expand_dims(image, 0)
+    return image
+
+
+def image_grid(images):
+    """
+    多个图片拼接成一张图片
+    Return a 5x5 grid of the MNIST images as a matplotlib figure."""
+    # Create a figure to contain the plot.
+    figure = plt.figure(figsize=(10, 10))
+    for i in range(25):
+        # Start next subplot.
+        plt.subplot(5, 5, i + 1, title='name')
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(images[i], cmap=plt.cm.binary)
+
+    return figure
